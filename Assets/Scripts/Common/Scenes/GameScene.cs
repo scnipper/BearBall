de360a9102b5a0623ded9196e1733077d19c16a2
Andrew Zhironkin <foxandreyy@gmail.com>
2021-09-18 21:22:57 +0300

    score

--- Assets/Scripts/Common/Scenes/GameScene.cs
+++ Assets/Scripts/Common/Scenes/GameScene.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Common.Scenes
 {
@@ -8,12 +9,27 @@ namespace Common.Scenes
         public Fruit fruit;
         public float minX = -9;
         public float maxX = 9;
+        public int startScore = 10;
+        public Text scoreText;
         private Transform parent;
 
+
+        private int scoreLeft;
+        private int scoreRight;
         private void Start()
         {
             parent = transform;
+
+            scoreLeft = startScore;
+            scoreRight = startScore;
+            UpdateScoreView();
+            
             StartCoroutine(FruitCreator());
+        }
+
+        private void UpdateScoreView()
+        {
+            scoreText.text = $"{scoreLeft} : {scoreRight}";
         }
 
         private IEnumerator FruitCreator()
@@ -23,7 +39,18 @@ namespace Common.Scenes
                 float xPos = Random.Range(minX,maxX);
                 var fruitInst = Instantiate(fruit, new Vector3(xPos,10,0), Quaternion.identity, parent);
                 fruitInst.IsRight = xPos > 0;
+                fruitInst.onGoal += OnGoalFruit;
                 yield return new WaitForSeconds(Random.Range(1.5f, 3f));
+            }
+        }
+
+        private void OnGoalFruit(bool isWrong)
+        {
+            if (isWrong)
+            {
+                scoreLeft--;
+                if (scoreLeft < 0) scoreLeft = 0;
+                UpdateScoreView();
             }
         }
     }
