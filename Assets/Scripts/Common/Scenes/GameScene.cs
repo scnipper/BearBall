d6135172d57bba1a3e44cfbe0be9d841d9cca7e8
Andrew Zhironkin <foxandreyy@gmail.com>
2021-09-18 21:50:59 +0300

    start and exit game

--- Assets/Scripts/Common/Scenes/GameScene.cs
+++ Assets/Scripts/Common/Scenes/GameScene.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Util;
@@ -14,20 +15,22 @@ namespace Common.Scenes
         public Text scoreText;
         private Transform parent;
 
+        [Header("Screens")]
         public GameObject pauseScreen;
+        public GameObject playScreen;
+        public GameObject mainScreen;
+        public GameObject playObjects;
+        
 
 
         private int scoreLeft;
         private int scoreRight;
+        private bool isFruitAdded;
+        private readonly List<Fruit> fruits = new List<Fruit>();
+
         private void Start()
         {
             parent = transform;
-
-            scoreLeft = startScore;
-            scoreRight = startScore;
-            UpdateScoreView();
-            
-            StartCoroutine(FruitCreator());
         }
 
         private void UpdateScoreView()
@@ -37,16 +40,49 @@ namespace Common.Scenes
 
         private IEnumerator FruitCreator()
         {
-            while (true)
+            fruits.Clear();
+            isFruitAdded = true;
+            while (isFruitAdded)
             {
                 float xPos = Random.Range(minX,maxX);
                 var fruitInst = Instantiate(fruit, new Vector3(xPos,10,0), Quaternion.identity, parent);
                 fruitInst.IsRight = xPos > 0;
                 fruitInst.onGoal += OnGoalFruit;
+                fruits.Add(fruitInst);
                 yield return new WaitForSeconds(Random.Range(1.5f, 3f));
             }
         }
 
+        public void PlayGame()
+        {
+            mainScreen.SetActive(false);
+            playScreen.SetActive(true);
+            playObjects.SetActive(true);
+            
+            scoreLeft = startScore;
+            scoreRight = startScore;
+            UpdateScoreView();
+            
+            StartCoroutine(FruitCreator());
+        }
+
+
+        public void ExitGame()
+        {
+            PauseGame(false);
+            isFruitAdded = false;
+            mainScreen.SetActive(true);
+            playScreen.SetActive(false);
+            playObjects.SetActive(false);
+            
+            foreach (var fr in fruits)
+            {
+                if (fr != null)
+                {
+                    Destroy(fr.gameObject);
+                }
+            }
+        }
         public void PauseGame(bool isPause)
         {
             P.isPauseGame = isPause;
